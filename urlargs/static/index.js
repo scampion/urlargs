f7bbe2c4f8a8611bdf6961ba1eb056237c807434
Sébastien Campion <sebastien.campion@inria.fr>
2018-04-23 23:39:21 +0200

    fix

--- urlargs/static/index.js
+++ urlargs/static/index.js
@@ -1,7 +1,7 @@
 define(function(){
 
     function _on_load(){
-	  IPython.notebook.kernel.execute("URL = ' + window.location + "'")
+          IPython.notebook.kernel.execute("URL = '" + window.location + "'"); 
     }
 
     return {load_ipython_extension: _on_load };
